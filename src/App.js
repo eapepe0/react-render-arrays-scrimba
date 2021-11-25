2b3a34529a85ffa6dc71bc6836c42e6d679f392f
eapepe0 <eapepe0@gmail.com>
2021-11-25 04:26:16 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Joke from "./Joke";
+import jokesData from "./JokeData";
+import "./styles.css";
+export default function App() {
+  /* recorre todo el array de objetos */
+  const jokeElements = jokesData.map((chiste) => {
+    /* devuelve chiste. */
+    return <Joke setup={chiste.setup} punchline={chiste.punchline} />;
+  });
+  return <div>{jokeElements}</div>;
+}
